32a30a9454a6b2c3785e1aeede954370227bfb42
nwd0907 <nwd0907@newbizstart.com>
2025-09-09 13:28:57 +0900

    12-01-library-icon

---
+++ class-example/src/app/section11/11-02-event-bubbling-stop-propagation/checkbox.tsx
@@ -0,0 +1,15 @@
+export default function Checkbox() {
+  const onClick2 = (event) => {
+    event.stopPropagation();
+    alert("2번 클릭");
+  };
+  const onClick3 = (event) => {
+    alert("3번 클릭");
+  };
+
+  return (
+    <span onClick={onClick2}>
+      <input type="checkbox" onClick={onClick3} />
+    </span>
+  );
+}
