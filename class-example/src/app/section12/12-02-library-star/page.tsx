f46f19423c64553f4d7ce969c5383f08c7674332
nwd0907 <nwd0907@newbizstart.com>
2025-09-14 17:40:51 +0900

    12-02-library-star 보완

--- class-example/src/app/section12/12-02-library-star/page.tsx
+++ class-example/src/app/section12/12-02-library-star/page.tsx
@@ -13,15 +13,17 @@ const App: React.FC = () => {
 
   return (
     <>
-      {/* 1단계 방식 */}
-      {/* 진짜 onChange */}
-      {/* <button onChange={onChangeInput}></button> */}
-
+      {/*  */}
+      {/*  */}
+      {/* === 1단계 방식 === */}
       {/* antd 개발자가 만든 가짜 onChange */}
       <Rate onChange={함수} value={value} />
       <span>점수: {value}점</span>
 
-      {/* 2단계 방식 */}
+      {/*  */}
+      {/*  */}
+      {/* === 2단계 방식 === */}
+      {/* 함수를 따로 분리하지 않아도 됨 */}
       <Rate
         onChange={(value: number) => {
           setValue(value);
@@ -30,7 +32,10 @@ const App: React.FC = () => {
       />
       <span>점수: {value}점</span>
 
-      {/* 3단계 방식 */}
+      {/*  */}
+      {/*  */}
+      {/* === 3단계 방식 === */}
+      {/* return은 종료를 의미하므로, 현재 로직에서는 있으나 없으나 상관 없음  */}
       <Rate
         onChange={(value: number) => {
           return setValue(value);
@@ -39,11 +44,19 @@ const App: React.FC = () => {
       />
       <span>점수: {value}점</span>
 
-      {/* 4단계 방식 */}
+      {/*  */}
+      {/*  */}
+      {/* === 4단계 방식 === */}
+      {/* 중괄호{}와 return 사이에 아무것도 없으면? 소괄호()로 변경 가능. 상황에 따라 소괄호() 생략 가능 */}
       <Rate onChange={(value) => setValue(value)} value={value} />
       <span>점수: {value}점</span>
 
-      {/* 5단계 방식: setValue뿐만 아니라, 어떤 함수던 상관없이 들어오는 변수(ex, value)와 들어가는 변수(ex, value)가 동일하면 생략 가능*/}
+      {/*  */}
+      {/*  */}
+      {/* === 5단계 방식 === */}
+      {/* setValue뿐만 아니라, 어떤 함수던 상관없이 들어오는 변수와 들어가는 변수가 동일하면 생략 가능 */}
+      {/* ex1) onChange={(value) => setValue(value)} === onChange{setValue} */}
+      {/* ex2) onClick={(event) => myFunc(event)} === onClick{myFunc} */}
       <Rate onChange={setValue} value={value} />
       <span>점수: {value}점</span>
     </>
