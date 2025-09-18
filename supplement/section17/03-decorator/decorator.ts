1ac1b4c0fc20c10ad8b496a0cb9bf06068461755
nwd0907 <nwd0907@newbizstart.com>
2025-09-18 14:46:32 +0900

    04-backend-db

---
+++ supplement/section17/03-decorator/decorator.ts
@@ -0,0 +1,12 @@
+function Entity(qqqqqqqqq: any) {
+  console.log("=======");
+  console.log(qqqqqqqqq);
+  console.log("=======");
+}
+
+@Entity
+class Board {
+  //   title: string;
+  //   content: string;
+  //   writer: string;
+}
