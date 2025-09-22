81d08f7fa5c89534bff0b4cfd5effc90986a8f66
nwd0907 <nwd0907@newbizstart.com>
2025-09-22 11:35:11 +0900

    18-01-image-upload

--- class-practice/src/app/layout.tsx
+++ class-practice/src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import ApiProvider from "@/commons/providers/13-04-api-provider";
 import Layout from "@/commons/layout";
+import ApiUploadProvider from "@/commons/providers/18-01-api-upload-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,9 +31,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ApiProvider>
+        {/* <ApiProvider>
           <Layout>{children}</Layout>
-        </ApiProvider>
+        </ApiProvider> */}
+
+        {/* 18-01 이미지업로드 */}
+        <ApiUploadProvider>
+          <Layout>{children}</Layout>
+        </ApiUploadProvider>
       </body>
     </html>
   );
