49830d1d0491b5ae974c077a436ded72e2355594
nwd0907 <nwd0907@newbizstart.com>
2025-09-25 11:52:41 +0900

    20-04-login-localstorage-check

--- class-practice/src/commons/providers/20-01-api-header-provider.tsx
+++ class-practice/src/commons/providers/20-01-api-header-provider.tsx
@@ -8,9 +8,16 @@ import {
 } from "@apollo/client";
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 import { useAccessTokenStore } from "../stores/20-01-access-token-store";
+import { useEffect } from "react";
 
 export default function ApiHeaderProvider(props) {
-  const { accessToken } = useAccessTokenStore();
+  // 프리렌더링 무시
+  useEffect(() => {
+    const result = localStorage.getItem("accessToken");
+    setAccessToken(result ?? "");
+  }, []);
+
+  const { accessToken, setAccessToken } = useAccessTokenStore();
 
   const uploadLink = createUploadLink({
     uri: "http://main-practice.codebootcamp.co.kr/graphql",
