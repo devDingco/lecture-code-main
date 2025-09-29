d5b58e995b62109bddbbae9b99a2e97f31013def
nwd0907 <nwd0907@newbizstart.com>
2025-09-29 11:50:08 +0900

    21-05-login-localstorage-check-hoc-success

---
+++ class-practice/src/app/section21/21-05-login-localstorage-check-hoc-success/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { withAuth } from "@/commons/hocs/21-05-with-auth";
+import { gql, useQuery } from "@apollo/client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const FETCH_USER_LOGGED_IN = gql`
+  query {
+    fetchUserLoggedIn {
+      _id
+      email
+      name
+    }
+  }
+`;
+
+function LoginSuccessPage() {
+  const { data } = useQuery(FETCH_USER_LOGGED_IN);
+
+  return <>{data?.fetchUserLoggedIn.name}님 환영합니다!</>;
+}
+
+export default withAuth(LoginSuccessPage);
