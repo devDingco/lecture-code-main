b3cd72801ea106d5399634f5da37b44adabe46f8
nwd0907 <nwd0907@newbizstart.com>
2025-11-14 14:58:09 +0900

    28-06-image-upload-preview-submit

--- class-practice/src/app/section28/28-05-image-upload-preview/page.tsx
+++ class-practice/src/app/section28/28-05-image-upload-preview/page.tsx
@@ -8,19 +8,19 @@ export default function ImageUploadPreviewPage() {
   const onChangeFile = (event) => {
     const file = event.target.files[0];
 
-    // 1. 임시URL 생성-1 => 내 컴퓨터에서만 가능(이미지주소: localhost로 시작함) - 용량 작음(간단한 주소 형식)
-    // const result = URL.createObjectURL(file);
-    // console.log(result);
-    // setImageUrl(result);
+    // 1. 임시URL 생성-1(과거방식) => 남의 컴퓨터에서도 가능(이미지주소: file형태의 text로 되어있음) - 용량 큼(주소 자체가 이미지 사이즈, ex 5MB)
+    // const fileReader = new FileReader();
+    // fileReader.readAsDataURL(file);
+    // fileReader.onload = (event) => {
+    //   console.log(event.target?.result);
+    //   if (typeof event.target?.result === "string")
+    //     setImageUrl(event.target?.result);
+    // };
 
-    // 2. 임시URL 생성-2 => 남의 컴퓨터에서도 가능(이미지주소: file형태의 text로 되어있음) - 용량 큼(주소 자체가 이미지 사이즈, ex 5MB)
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(file);
-    fileReader.onload = (event) => {
-      console.log(event.target?.result);
-      if (typeof event.target?.result === "string")
-        setImageUrl(event.target?.result);
-    };
+    // 2. 임시URL 생성-2(최신방식) => 내 컴퓨터에서만 가능(이미지주소: localhost로 시작함) - 용량 작음(간단한 주소 형식)
+    const result = URL.createObjectURL(file);
+    console.log(result);
+    setImageUrl(result);
   };
 
   return (
