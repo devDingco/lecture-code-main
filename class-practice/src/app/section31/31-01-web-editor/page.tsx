2cff34162e8ad6bfadd405c43a93cf6fd650d8be
nwd0907 <nwd0907@newbizstart.com>
2025-11-25 12:08:44 +0900

    31-01-web-editor

---
+++ class-practice/src/app/section31/31-01-web-editor/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+// import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import dynamic from "next/dynamic";
+
+const ReactQuill = dynamic(() => import("react-quill"), {
+  ssr: false,
+});
+
+export default function WebEditorPage() {
+  // ReactQuill 개발자가 만든 onChange => 따라서, event 안들어옴
+  const onChangeContent = (value) => {
+    console.log(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    // code-spliting (코드 스플리팅)
+    const { Modal } = await import("antd");
+    Modal.success({ content: "게시글 등록에 성공하였습니다!!" });
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      작성자: <input type="text" />
+      <br />
+      비밀번호: <input type="password" />
+      <br />
+      제목: <input type="text" />
+      <br />
+      내용: <ReactQuill onChange={onChangeContent} />
+      <br />
+      <button>등록하기</button>
+    </form>
+  );
+}
