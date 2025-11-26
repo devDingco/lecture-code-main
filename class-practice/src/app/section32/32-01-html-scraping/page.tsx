3cae7ad33ab2332bb52d73891b385a957e7df90d
nwd0907 <nwd0907@newbizstart.com>
2025-11-26 14:55:16 +0900

    32-01-html-scraping

---
+++ class-practice/src/app/section32/32-01-html-scraping/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function HtmlScrapingPage() {
+  const onClickScraping = async () => {
+    // fetch("https://www.naver.com") // CORS문제 발생
+
+    const result = await fetch(
+      "https://vibe-coding-supabase-kwtm.vercel.app/magazines"
+    );
+    const data = await result.text();
+    console.log(data);
+  };
+
+  return <button onClick={onClickScraping}>긁어오기</button>;
+}
